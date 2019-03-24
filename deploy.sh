d6ee1f1571c15899a4216d247ad500a7ba6993ba
sillyhat <xushikuan1@163.com>
2019-03-24 23:29:57 +0800

    add CRUD

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 go clean
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build main.go
 docker build -t short-url .
-docker tag short-url:latest xushikuan/short-url:1.0
-docker push xushikuan/short-url:1.0
+docker tag short-url:latest xushikuan/short-url:1.1
+docker push xushikuan/short-url:1.1
